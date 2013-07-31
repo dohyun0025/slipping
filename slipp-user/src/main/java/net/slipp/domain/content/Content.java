b5fc78356896ca884a4260be05ab401a2a545865
dohyun0025 <c3098051@gmail.com>
2013-07-31 17:11:05 +0900

    1.0 wednesday

---
+++ slipp-user/src/main/java/net/slipp/domain/content/Content.java
@@ -0,0 +1,46 @@
+package net.slipp.domain.content;
+
+public class Content {
+	private static int number=0;//Map에서 유니크한 키넘버를 위한 변수 
+	private int index;			//글의 번호 
+	private String subject;     //제목 
+	private String note;		//내용 
+	private String time;		//시간 
+	
+	public Content(){
+	}
+	public Content(String string, String string2, String string3) {
+		// TODO Auto-generated constructor stub
+		this.index = number;
+		this.subject = string;
+		this.note = string2;
+		this.time = string3;
+	}
+
+	public int getIndex() {
+		number++;
+		setIndex(number);
+		return index;
+	}
+	public void setIndex(int index) {
+		this.index = index;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getNote() {
+		return note;
+	}
+	public void setNote(String note) {
+		this.note = note;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+}
